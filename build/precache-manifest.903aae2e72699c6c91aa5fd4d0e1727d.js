19e3371bac36956eaca886fd57992994276569de
Vinayak Hegde <vinayak.hegde@intuitivebi.com>
2019-04-04 00:51:38 +0100

    Build

---
+++ build/precache-manifest.903aae2e72699c6c91aa5fd4d0e1727d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "94872cf7e81768d148a3",
+    "url": "/react-portfolio/static/js/runtime~main.f974a8eb.js"
+  },
+  {
+    "revision": "8ca707b6758fff528808",
+    "url": "/react-portfolio/static/js/main.894c4632.chunk.js"
+  },
+  {
+    "revision": "2f6c97cab20cffac5f8f",
+    "url": "/react-portfolio/static/js/2.40fed8bf.chunk.js"
+  },
+  {
+    "revision": "8ca707b6758fff528808",
+    "url": "/react-portfolio/static/css/main.c0f234f3.chunk.css"
+  },
+  {
+    "revision": "cacd8731e3aefac639db49955d69d159",
+    "url": "/react-portfolio/index.html"
+  }
+];
