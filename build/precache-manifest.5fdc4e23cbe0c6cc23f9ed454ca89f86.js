aa9bc546d57cac70006d7ce35dcc36c5b8360edb
Vinayak Hegde <vinayak.hegde@intuitivebi.com>
2019-04-03 22:04:04 +0100

    homepage path correction

---
+++ build/precache-manifest.5fdc4e23cbe0c6cc23f9ed454ca89f86.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "94872cf7e81768d148a3",
+    "url": "/react-portfolio/static/js/runtime~main.f974a8eb.js"
+  },
+  {
+    "revision": "fa7ecfe9b3266c75ebac",
+    "url": "/react-portfolio/static/js/main.85f9d96c.chunk.js"
+  },
+  {
+    "revision": "3f4ade220ee875194238",
+    "url": "/react-portfolio/static/js/2.7b23cc03.chunk.js"
+  },
+  {
+    "revision": "fa7ecfe9b3266c75ebac",
+    "url": "/react-portfolio/static/css/main.d63a5578.chunk.css"
+  },
+  {
+    "revision": "e7e34ebb4066448ceedb163b8d109593",
+    "url": "/react-portfolio/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.3edabf7e556f106301518b494ad051bd.js"
+  "/react-portfolio/precache-manifest.5fdc4e23cbe0c6cc23f9ed454ca89f86.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/portfolio/index.html", {
+workbox.routing.registerNavigationRoute("/react-portfolio/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
