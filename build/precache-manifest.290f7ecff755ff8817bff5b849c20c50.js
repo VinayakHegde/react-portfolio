72cd418d087fe287d5157a455c176acfeeb02fb6
Vinayak Hegde <vinayak.hegde@intuitivebi.com>
2019-04-13 23:12:24 +0100

    Build for gh-pages

---
+++ build/precache-manifest.290f7ecff755ff8817bff5b849c20c50.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a28a89b16de969e7a1e01054886edddf",
+    "url": "/react-portfolio/static/media/profile.a28a89b1.pdf"
+  },
+  {
+    "revision": "94872cf7e81768d148a3",
+    "url": "/react-portfolio/static/js/runtime~main.f974a8eb.js"
+  },
+  {
+    "revision": "4bece06e402229a4a33b",
+    "url": "/react-portfolio/static/js/main.a9796b21.chunk.js"
+  },
+  {
+    "revision": "068e165e46f69fc38c0d",
+    "url": "/react-portfolio/static/js/2.730be731.chunk.js"
+  },
+  {
+    "revision": "4bece06e402229a4a33b",
+    "url": "/react-portfolio/static/css/main.816a269f.chunk.css"
+  },
+  {
+    "revision": "afe2e586647a43312b99e1892e2e722a",
+    "url": "/react-portfolio/index.html"
+  }
+];
