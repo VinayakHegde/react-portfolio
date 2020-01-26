3ee9a00cf6f0ae475df35be0506ecbd3ca79016c
Vinayak Hegde <vinayak.nandi@gmail.com>
2020-01-26 22:49:06 +0000

    USer card refactoring

--- src/Components/Cards/UserCard/UserCard.jsx
+++ src/Components/Cards/UserCard/UserCard.jsx
@@ -3,9 +3,11 @@ import PropTypes from "prop-types";
 
 import { ICONTYPE } from "Helpers/Enums";
 import { UserDetails } from "Helpers/DataValidator";
-import resume from "Data/vinayak.nandi@gmail.com.doc";
 import Button from "Components/Button";
+import Spinner from "Components/Spinner";
 import Icon from "Components/Icon";
+import {pause} from 'Helpers/utils';
+import useCustomState from 'hooks/useCustomState';
 import "./UserCard.scss";
 
 const UserCard = ({ cssClass }) => {
@@ -13,8 +15,24 @@ const UserCard = ({ cssClass }) => {
   const knownAs = UserDetails.KnownAs.length
     ? "(Known as ".concat(UserDetails.KnownAs, ")")
     : "";
+  const [isLoading, setIsLoading] = useCustomState(false);
+  const downloadResume = async () => {
+    setIsLoading(true);
+    await pause();
+    import(/*webpackChunkName: "resume" */ 'Assets/Data/vinayak.nandi@gmail.com.doc').then(({default: resume}) => {
+      const link = document.createElement('a');
+      link.href = resume;
+      link.rel = "noopener noreferrer";
+      link.style.display = 'none';
+      document.body.appendChild(link);
+      link.click();
+      link.remove();
+      setIsLoading(false);
+    });
+  };
   return (
     <div className={cssClassName}>
+      {isLoading && <Spinner />}
       <div className="user__details user__name user__friendly-name">
         {UserDetails.FriendlyName}
       </div>
@@ -29,9 +47,8 @@ const UserCard = ({ cssClass }) => {
       </div>
       <div className="user__details download__wrapper">
         <Button {...{
-          value: resume,
+          fnCallback: downloadResume,
           btnClass: "download__button resume",
-          linkClass: "button-link",
           tooltip: "Download resume",
           type: ICONTYPE.FILEDOWNLOAD
         }}>
