8f9add26e42ccd802185d503fc70fb6ec269aea9
Vinayak Hegde <vinayak.nandi@gmail.com>
2020-01-26 22:38:38 +0000

    Spinner indicator while loading resources

---
+++ src/Components/Spinner/Spinner.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Spinner.scss';
+
+const Spinner = () => (
+  <div className="Spinner">
+    <span></span>	
+    <span></span>
+    <span></span>
+    <span></span>
+    <span></span>
+  </div>
+);
+
+export default Spinner;
